a1377fd028cda7b831d5c797db2d729dfbca10cb
PolloTrick <A23310317@ceti.mx>
2026-03-15 10:37:45 -0600

    Agreamos modul1, y literales tema 2 de curso

---
+++ Literales.py
@@ -0,0 +1,27 @@
+# --- Sistemas de Numeración ---
+
+# Imprime un encabezado para la sección de números octales
+print("Numeros Octagonales")
+# Imprime el valor decimal de 123 en base 8 (Octal). 
+# El prefijo '0o' indica que es octal. Resultado: 83
+print(0o123)
+
+# Imprime un encabezado para la sección de números hexadecimales
+print("Numeros hexadecimales")
+# Imprime el valor decimal de 123 en base 16 (Hexadecimal).
+# El prefijo '0x' indica que es hexadecimal. Resultado: 291
+print(0x123)
+
+# --- Lógica con Booleanos ---
+
+# Imprime un encabezado para la sección de comparaciones lógicas
+print("valores booleanos")
+
+# En Python, True equivale internamente a 1 y False a 0.
+# Compara si 1 es mayor que 0. Resultado: True
+print(True > False)
+
+# Compara si 1 es menor que 0. Resultado: False
+print(True < False)
+
+print("Final")
